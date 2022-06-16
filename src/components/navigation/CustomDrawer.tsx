9afde0c342023d54d39746dc977438a82af03474
priyanka-codeprism <priyanka.pradhan@codeprism.in>
2022-06-16 20:25:26 +0530

    first commit

---
+++ src/components/navigation/CustomDrawer.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+
+const CustomDrawer = () => {
+  return (
+    <View>
+      <Text>CustomDrawer</Text>
+    </View>
+  )
+}
+
+export default CustomDrawer
